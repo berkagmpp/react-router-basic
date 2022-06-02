d9934bd7602a65b59372510a40706f83767a1461
berkagmpp <berkagmpp@gmail.com>
2022-06-02 14:49:32 +1200

    Feat: Add and Extract dynamic Routes with Params Change a path segment - Route path of ProductDetail to /products/:productId in App.js Add useParams(react-router-dom) in ProductDetail.js Add Link to={}(react-router-dom) in Products.js Ref: #31336a9e79da016468341cbe106a8b601f67c6ff

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,25 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
     return (
         <section>
             <h1>The Products Page!</h1>
             <ul>
-                <li>Whittakers Cocoa 92% Ghana 100g</li>
-                <li>Spam Lite 340g</li>
-                <li>Haagen Lager 330ml 12Pk Btl</li>
+                <li>
+                    <Link to="/products/p1">
+                        Whittakers Cocoa 92% Ghana 100g
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/products/p2">
+                        Spam Lite 340g
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/products/p3">
+                        Haagen Lager 330ml 12Pk Btl
+                    </Link>
+                </li>
             </ul>
         </section>
     );
